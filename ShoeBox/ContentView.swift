4505becc9ca5d7edffd5e046096e4ba20d38dbe2
Yukio Murakami <m_yukio@mac.com>
2025-04-28 15:43:11 +0900

    新規プロジェクト

---
+++ ShoeBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoeBox
+//
+//  Created by 村上幸雄 on 2025/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
